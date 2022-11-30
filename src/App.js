d15acf1cc31925537bc929422ac4db0a53ab59cb
Yalun Zhang <yalun.zhang@warwick.ac.uk>
2022-11-30 12:59:41 +0000

    added privacy policy

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/privacy"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Privacy Page
         </a>
       </header>
     </div>
